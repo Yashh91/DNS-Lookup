fe4a3f39db8cad6fccaccea8c11c5492b65a50f1
Yash <yashmurumkar07@gmail.com>
2026-08-02 02:42:06 +0530

    Add banner function for DNS lookup tool

---
+++ banner.py
@@ -0,0 +1,8 @@
+def banner():
+    print("=" * 50)
+    print("             DNS LOOKUP TOOL")
+    print("=" * 50)
+    print("Author  : Yashh91")
+    print("Version : 1.0")
+    print("GitHub  : https://github.com/Yashh91")
+    print("=" * 50)
